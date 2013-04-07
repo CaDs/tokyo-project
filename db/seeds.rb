00e5700355e2d1805112f33eb2fea232259d7371
cads <cdonderis@gmail.com>
2013-04-07 17:39:28 +0900

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,28 @@
 # We provide you a basic shell for interaction with the end user.
 # So try some code like below:
 #
-#   name = shell.ask("What's your name?")
-#   shell.say name
-#
-# email     = shell.ask "Which email do you want use for logging into admin?"
-# password  = shell.ask "Tell me the password to use:"
+name = shell.ask("What's your name?")
+shell.say name
 
-# shell.say ""
+email     = shell.ask "Which email do you want use for logging into admin?"
+password  = shell.ask "Tell me the password to use:"
 
-# account = Account.create(:email => email, :name => "Foo", :surname => "Bar", :password => password, :password_confirmation => password, :role => "admin")
+shell.say ""
 
-# if account.valid?
-#   shell.say "================================================================="
-#   shell.say "Account has been successfully created, now you can login with:"
-#   shell.say "================================================================="
-#   shell.say "   email: #{email}"
-#   shell.say "   password: #{password}"
-#   shell.say "================================================================="
-# else
-#   shell.say "Sorry but some thing went wrong!"
-#   shell.say ""
-#   account.errors.full_messages.each { |m| shell.say "   - #{m}" }
-# end
+account = Account.create(:email => email, :name => "Foo", :surname => "Bar", :password => password, :password_confirmation => password, :role => "admin")
+
+if account.valid?
+  shell.say "================================================================="
+  shell.say "Account has been successfully created, now you can login with:"
+  shell.say "================================================================="
+  shell.say "   email: #{email}"
+  shell.say "   password: #{password}"
+  shell.say "================================================================="
+else
+  shell.say "Sorry but some thing went wrong!"
+  shell.say ""
+  account.errors.full_messages.each { |m| shell.say "   - #{m}" }
+end
 
 shell.say "Generating Wards"
 wards_names = %w[Adachi Arakawa Bunkyō Chiyoda Chūō Edogawa Itabashi Katsushika Kita Kōtō Meguro Minato Nakano Nerima Ōta Setagaya Shibuya Shinagawa Shinjuku Suginami Sumida Taitō Toshima]
