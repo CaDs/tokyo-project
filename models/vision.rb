0d652471d1554c1c5464f66ae1d4bfd9e5568fff
cads <cdonderis@gmail.com>
2013-04-24 17:51:39 +0900

    added default image for landing blocks

--- models/vision.rb
+++ models/vision.rb
@@ -3,4 +3,6 @@ class Vision < ActiveRecord::Base
   belongs_to :account
   has_many :pictures
 
+  DEFAULT_MEDIUM_PICTURE_URL = "http://farm9.staticflickr.com/8300/7814117342_b345e98c65_n.jpg"
+
 end
