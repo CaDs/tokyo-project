0f87752cd3a0339be23eb733012c89616966b71e
cads <cdonderis@gmail.com>
2013-06-01 20:16:22 +0900

    fragment cache powah

--- app/controllers/visions.rb
+++ app/controllers/visions.rb
@@ -4,22 +4,22 @@ TokyoProject.controllers :visions do
     ActiveRecord::Base.connection.close
   end
 
-  get :show, :map => '/visions/:id(/:pid)', :cache => false do
+  get :show, :map => '/visions/:id(/:pid)' do
     key = "vision_show_#{params[:id]}"
     key += "_#{pid}" if params[:pid]
-    cache_key key
-    expires_in(Padrino.env.to_s == "production" ? 3600 : 1)
-
-    @vision = Vision.find(params[:id]) rescue nil
-    if @vision
-      @pictures = @vision.published_pictures
-      @picture = @pictures.find(params[:pid]) if params[:pid] rescue nil
-      @picture ||= @vision.pictures.first
-      render 'visions/show'
-    else
-      flash[:notice] = "Vision not founded"
-      redirect url(:areas, :index)
-    end
+    cache(key: key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 1)) do
+      # cache_key key
+      @vision = Vision.find(params[:id]) rescue nil
+      if @vision
+        @pictures = @vision.published_pictures
+        @picture = @pictures.find(params[:pid]) if params[:pid] rescue nil
+        @picture ||= @vision.pictures.first
+        render 'visions/show'
+      else
+        flash[:notice] = "Vision not founded"
+        redirect url(:areas, :index)
+      end
+    end #cache
   end
 
 end
