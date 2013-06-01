a4c4db69042265005593519ad368a435be8af8b7
cads <cdonderis@gmail.com>
2013-06-01 17:15:21 +0900

    removing unpublished pictures from the rss feed

--- app/controllers/rss.rb
+++ app/controllers/rss.rb
@@ -1,6 +1,6 @@
 TokyoProject.controllers :rss do
   get :index, :provides => [:rss] do
-    @pictures = Picture.all
+    @pictures = Picture.where(:is_published => true)
     render 'rss/index'
   end
 end
