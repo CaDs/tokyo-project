ba99514f2755306ac00da8c30de8daa1659ab33c
cads <cdonderis@gmail.com>
2013-06-04 14:40:36 +0900

    touch

--- app/controllers/visions.rb
+++ app/controllers/visions.rb
@@ -5,7 +5,7 @@ TokyoProject.controllers :visions do
   end
 
   get :show, :map => '/visions/:id(/:pid)' do
-    @vision = Vision.find(params[:id]) rescue nil
+    @vision = Vision.find(params[:id]) rescue nil 
     if @vision
       @pictures = @vision.published_pictures
       @picture = @pictures.find(params[:pid]).first if params[:pid] rescue nil
