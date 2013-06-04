4abb5bf8de979be9892918e7aba5007bbf9d5570
cads <cdonderis@gmail.com>
2013-06-04 14:34:43 +0900

    removing cache for visions for now

--- app/controllers/visions.rb
+++ app/controllers/visions.rb
@@ -5,21 +5,16 @@ TokyoProject.controllers :visions do
   end
 
   get :show, :map => '/visions/:id(/:pid)' do
-    key = "vision_show_#{params[:id]}"
-    key += "_#{params[:pid]}" if params[:pid]
-    cache(key: key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 1)) do
-      # cache_key key
-      @vision = Vision.find(params[:id]) rescue nil
-      if @vision
-        @pictures = @vision.published_pictures
-        @picture = @pictures.find(params[:pid]).first if params[:pid] rescue nil
-        @picture ||= @vision.pictures.first
-        render 'visions/show'
-      else
-        flash[:notice] = "Vision not founded"
-        redirect url(:areas, :index)
-      end
-    end #cache
+    @vision = Vision.find(params[:id]) rescue nil
+    if @vision
+      @pictures = @vision.published_pictures
+      @picture = @pictures.find(params[:pid]).first if params[:pid] rescue nil
+      @picture ||= @vision.pictures.first
+      render 'visions/show'
+    else
+      flash[:notice] = "Vision not founded"
+      redirect url(:areas, :index)
+    end
   end
 
 end
