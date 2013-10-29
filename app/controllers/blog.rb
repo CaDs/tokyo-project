2b1f0c0edd890dd11685112325a85deb01878f48
cads <cdonderis@gmail.com>
2013-10-29 21:49:08 +0900

    development cache time restored to

--- app/controllers/blog.rb
+++ app/controllers/blog.rb
@@ -2,7 +2,7 @@ TokyoProject::TokyoProject.controllers :blog do
 
   get :index do
     key = "blog"
-    cache(key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 20)) do
+    cache(key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 1)) do
       @posts = Post.where("is_published = true").order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
       render 'blog/index'
     end
@@ -10,7 +10,7 @@ TokyoProject::TokyoProject.controllers :blog do
 
   get :show, :with => :id do
     key = "blog_show_#{params[:id]}"
-    cache(key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 20)) do
+    cache(key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 1)) do
       @post = Post.find(params[:id])
       render 'blog/show'
     end
--- app/controllers/maps.rb
+++ app/controllers/maps.rb
@@ -3,7 +3,7 @@ TokyoProject::TokyoProject.controllers :maps do
   get :show, :map => '/maps/:id' do
     key = "maps_show_#{params[:id]}"
 
-    cache(key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 10)) do
+    cache(key, expires_in: (Padrino.env.to_s == "production" ? 3600 : 1)) do
       @vision = Vision.find(params[:id])
       map_data = @vision.map_data
       @static_map_url = map_data['static_url']
