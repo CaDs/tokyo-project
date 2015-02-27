5fc3f652b7229daf89715e948f772e4d834ac002
Carlos Donderis <cdonderis@gmail.com>
2015-02-27 15:43:36 +0900

    adding auto update for new title

--- models/area.rb
+++ models/area.rb
@@ -2,9 +2,16 @@ class Area < ActiveRecord::Base
   belongs_to :ward
   has_many :visions
 
+  before_save :update_url_title
+
   def clear_cache
     TokyoProject.cache.delete("root_path")
     TokyoProject.cache.delete("areas")
     TokyoProject.cache.delete("area_show_#{self.id}")
+  end
+
+  def update_url_title
+    tmp_title = self.name.gsub(',', '').gsub('.', '').split(' ').collect{|word| word.capitalize}.join('').underscore rescue nil
+    self.url_title= URI::encode(tmp_title)
   end
 end
--- models/vision.rb
+++ models/vision.rb
@@ -1,9 +1,12 @@
+require 'open-uri'
+
 class Vision < ActiveRecord::Base
   belongs_to :area
   belongs_to :account
   has_many :pictures
 
   DEFAULT_MEDIUM_PICTURE_URL = "http://farm9.staticflickr.com/8300/7814117342_b345e98c65_m.jpg"
+  before_save :update_url_title
 
   def published_pictures
     pictures.order("created_at DESC").find_all{|p| p.is_published }
@@ -41,6 +44,11 @@ class Vision < ActiveRecord::Base
     TokyoProject.cache.delete("vision_show_#{self.id}")
     TokyoProject.cache.delete("maps_show_#{self.id}")
     area.clear_cache
+  end
+
+  def update_url_title
+    tmp_title = self.title.gsub(',', '').gsub('.', '').split(' ').collect{|word| word.capitalize}.join('').underscore rescue nil
+    self.url_title= URI::encode(tmp_title)
   end
 
 end
