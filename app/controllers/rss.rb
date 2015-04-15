fac9e774e28289a685a56b49933e0c64fac5c485
Carlos Donderis <cdonderis@gmail.com>
2015-04-15 13:07:10 +0900

    showing only latest 20 pictures for rss

--- app/controllers/rss.rb
+++ app/controllers/rss.rb
@@ -1,8 +1,9 @@
 TokyoProject.controllers :rss do
   get :index, :provides => [:rss] do
     @cached_pictures = cache("rss_pictures", expires_in: (Padrino.env.to_s == "production" ? 3600 : 5)) do
-      @pictures_array = Picture.published.last(50).collect{|picture| [picture, picture.vision]}
+      @pictures_array = Picture.published.last(20).collect{|picture| [picture, picture.vision]}
     end
+
     @cached_posts = cache("rss_posts", expires_in: (Padrino.env.to_s == "production" ? 3600 : 5)) do
       @posts = Post.where(:is_published => true)
     end
