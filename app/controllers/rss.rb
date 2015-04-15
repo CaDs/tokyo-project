0d796ee5a256cbdd2eaf73e79483f3718fc7bfc5
Carlos Donderis <cdonderis@gmail.com>
2015-04-15 12:19:55 +0900

    caching heavy process for rss generation

--- app/controllers/rss.rb
+++ app/controllers/rss.rb
@@ -1,7 +1,11 @@
 TokyoProject.controllers :rss do
   get :index, :provides => [:rss] do
-    @pictures = Picture.published
-    @posts = Post.where(:is_published => true)
+    @pictures = cache("rss_pictures", expires_in: (Padrino.env.to_s == "production" ? 3600 : 5)) do
+      Picture.published
+    end
+    @posts = cache("rss_posts", expires_in: (Padrino.env.to_s == "production" ? 3600 : 5)) do
+      @posts = Post.where(:is_published => true)
+    end
     render 'rss/index'
   end
 end
